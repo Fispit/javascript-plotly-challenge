89ae81badc991820fc825a58c1b9536ff2bdc177
Fispit <sdelcos@gmail.com>
2021-07-02 15:24:02 -0500

    Working on updating metadata section

--- static/js/app.js
+++ static/js/app.js
@@ -1,17 +1,30 @@
-var dropdown=d3.select("#selDataset");
+var dropdown_menu=d3.select("#selDataset");
 var sample_metadata=d3.select("#sample-metadata");
 var bubble=d3.select("#bubble");
 var bar=d3.select("#bar");
 var gauge=d3.select("#gauge");
 
 
-function optionChanged(){
-
-
-}
 
 
 d3.json("./data/samples.json").then((data)=>{
+
+    //saving names to aa variable for dropdown, data type change to int just in case
+    var names=data.names.map((value)=>parseInt(value));
+    //populating dropdown menu
+    names.forEach((name)=>{dropdown_menu.append("option").text(name)});
+    var startname=dropdown_menu.property('value');
+
+
+
+
+
+
+    var metadata=data.metadata;
+    var dispmeta=metadata.find((metadata)=>metadata.id==startname);
+    updatemetadata(dispmeta);
+    
+    var samples=data.samples;
 
 
 
@@ -19,6 +32,22 @@ d3.json("./data/samples.json").then((data)=>{
     
 });
 
+
+
+function updatemetadata(data){
+    sample_metadata.text("");
+    console.log(data);
+    data.each((k,v)=>{
+        sample_metadata.append("row").value(`${k}: ${v}`)
+    })
+
+
+}
+
+function optionChanged(){
+
+
+}
 
 
 //"id" "ethnicity" "gender" "age" "location" "bbtype" "wfreq"
