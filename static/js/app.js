0975a7e6aa330afb649924ed4ca5aa679e7beab6
Fispit <sdelcos@gmail.com>
2021-07-02 13:39:33 -0500

    Added initial data structure.

---
+++ static/js/app.js
@@ -0,0 +1,24 @@
+var dropdown=d3.select("#selDataset");
+var sample_metadata=d3.select("#sample-metadata");
+var bubble=d3.select("#bubble");
+var bar=d3.select("#bar");
+var gauge=d3.select("#gauge");
+
+
+function optionChanged(){
+
+
+}
+
+
+d3.json("./data/samples.json").then((data)=>{
+
+
+
+
+    
+});
+
+
+
+//"id" "ethnicity" "gender" "age" "location" "bbtype" "wfreq"
